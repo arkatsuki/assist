6b4ce0be1dc4951dbf8857915dc1e042527dbd8b
smileliving <291611696@qq.com>
2018-04-12 18:53:30 +0800

    2018041201

--- assist_util.py
+++ assist_util.py
@@ -44,7 +44,7 @@ def binary_search(find, list1) :
     return -1
 
 
-def binary_search(new_num, num_list) :
+def binary_search_asc(new_num, num_list) :
     '''
     self 二分插入排序
     :param new_num:
@@ -53,29 +53,120 @@ def binary_search(new_num, num_list) :
     '''
     low = 0
     high = len(num_list)
+    if high==0:
+        num_list.append(new_num)
+        return
+        pass
     while low <= high :
         mid = (low + high)//2
+        print('lhm',low,high,mid)
+        # print('num_list:',num_list)
+        if num_list[mid] == new_num:
+            num_list.insert(mid, new_num)
+            print('num_list:', num_list)
+            break
+        # 左半边
+        elif num_list[mid] > new_num :
+            high = mid - 1
+            if low == high:
+                num_list.insert(low + 1, new_num)
+                print('num_list:', num_list)
+                break
+                pass
+        #右半边
+        else :
+            low = mid + 1
+            if low == high:
+                num_list.insert(low, new_num)
+                print('num_list:', num_list)
+                break
+                pass
+            pass
+
+        if high<low:
+            num_list.insert(low, new_num)
+            print('num_list:',  num_list)
+            break
+            pass
+    return num_list
+
+
+def binary_search_desc(new_num, num_list) :
+    '''
+    self 二分插入排序
+    :param new_num:
+    :param num_list:
+    :return:
+    '''
+    low = 0
+    high = len(num_list)
+    if high==0:
+        num_list.append(new_num)
+        return
+        pass
+    while low <= high :
+        mid = (low + high)//2
+        print('lhm',low,high,mid)
         if num_list[mid] == new_num:
             num_list.insert(mid, new_num)
             break
         # 左半边
-        elif num_list[mid] > new_num :
-            high = mid -1
+        elif num_list[mid] < new_num :
+            high = mid - 1
+            if low == high:
+                if num_list[low]==new_num:
+                    num_list.insert(low, new_num)
+                elif num_list[low]>new_num:
+                    num_list.insert(low+1, new_num)
+                else:
+                    num_list.insert(low, new_num)
+                    pass
+                print('L num_list:', num_list)
+                break
+                pass
         #右半边
         else :
             low = mid + 1
+            if low == high:
+                if low>=len(num_list):
+                    num_list.insert(low-1, new_num)
+                    pass
+                if num_list[low]==new_num:
+                    num_list.insert(low, new_num)
+                elif num_list[low]>new_num:
+                    num_list.insert(low+1, new_num)
+                else:
+                    num_list.insert(low, new_num)
+                    pass
+                print('R num_list:', num_list)
+                break
+                pass
             pass
-        if high<=low:
+
+        if high<low:
             num_list.insert(low, new_num)
+            print('< num_list:',  num_list)
             break
             pass
     return num_list
 
 
 if __name__ == "__main__":
-    list1 = [1,3,5,7,9,11,13,15,17,19]
+    # list1 = [1,3,5,7,9,11,13,15,17,19]
     # list2 = binary_search(9, list1)
     # list2 = binary_search(10, list1)
-    list2 = binary_search(11, list1)
+    # list2 = binary_search_desc(11, list1)
+    # list1 = [1, 11, 13, 15, 7, 5, 3, 2, 1, 17, 19, 10]
+
+    list1 = [11, 1, 13, 15, 7, 5, 3, 2, 1, 3, 5, 17, 19, 10, 100]
+    # list1 = [1, 11, 13, 15, 7, 5, 3, 2, 1, 3, 5, 17, 19, 10]
+    list2 = []
+    for elem in list1:
+        binary_search_desc(elem, list2)
+        # binary_search_asc(elem, list2)
+        pass
+
     print('list2:',list2)
+
+    # print(1//2)
     pass
--- excel_filter.py
+++ excel_filter.py
@@ -79,35 +79,154 @@ def filter_rule5(data_dict):
     :return:
     """
     rs_dict = {}  # 用于返回
+    title_column1 = []
+    title_column2 = []
 
     for sheet_name, sheet_data in data_dict.items():
         rs_dict[sheet_name] = []
-        rs_dict[sheet_name].append(sheet_data[0])
-        rs_dict[sheet_name].append(sheet_data[3])
+        title_column1 = sheet_data[0]
+        title_column2 = sheet_data[3]
         for row_list in sheet_data:
-            print('row_list[10]:',row_list[10])
-            # 正则表达式，包含A或者B
-            if type(row_list[10]) == str and re.match('A|B', row_list[10]):
-                print('find A')
-                rs_dict[sheet_name].append(row_list)
-                pass
+            binary_insert_list_desc(row_list, rs_dict[sheet_name], 8)
             pass
         pass
+    # .xls最多65536
+    rs_dict[sheet_name] = rs_dict[sheet_name][0:60000]
+    rs_dict[sheet_name].insert(0, title_column1)
+    rs_dict[sheet_name].insert(1, title_column2)
+    # if len(rs_dict[sheet_name])>60000:
+    #     rs_dict[sheet_name] = rs_dict[sheet_name][0:60000]
+    #     pass
     return rs_dict
+
+
+def binary_insert_list_desc11111(new_elem, list, index) :
+    data = new_elem[index]
+    if not isinstance(data,float):
+        try:
+            data = float(data)
+            pass
+        except Exception:
+            # print('data:',data)
+            return
+        pass
+    low = 0
+    high = len(list)
+    if high==0:
+        list.append(new_elem)
+        return
+        pass
+    while low <= high :
+        mid = (low + high)//2
+        if float(list[mid][index]) == data:
+            list.insert(mid, new_elem)
+            break
+        # 左半边
+        elif float(list[mid][index]) < data :
+            high = mid - 1
+            if low == high:
+                list.insert(low + 1, new_elem)
+                break
+                pass
+        #右半边
+        else :
+            low = mid + 1
+            if low == high:
+                list.insert(low, new_elem)
+                break
+                pass
+            pass
+
+        if low==high and low==0:
+            list.insert(low + 1, new_elem)
+            break
+            pass
+
+        if high<low:
+            list.insert(low, new_elem)
+            print('num_list:', list)
+            break
+            pass
+    return list
+
+
+def binary_insert_list_desc(new_elem, num_list, index):
+    data = new_elem[index]
+    if not isinstance(data, float):
+        try:
+            data = float(data)
+            pass
+        except Exception:
+            # print('data:',data)
+            return
+        pass
+    low = 0
+    high = len(num_list)
+    if high==0:
+        num_list.append(new_elem)
+        return
+        pass
+    while low <= high :
+        mid = (low + high)//2
+        # print('lhm', low, high, mid)
+        if float(num_list[mid][index]) == data:
+            num_list.insert(mid, new_elem)
+            break
+        # 左半边
+        elif float(num_list[mid][index]) < data :
+            high = mid - 1
+            if low == high:
+                if float(num_list[low][index]) == data:
+                    num_list.insert(low, new_elem)
+                elif float(num_list[low][index])>data:
+                    num_list.insert(low + 1, new_elem)
+                else:
+                    num_list.insert(low, new_elem)
+                    pass
+                break
+                pass
+        #右半边
+        else :
+            low = mid + 1
+            if low == high:
+                if low>=len(num_list):
+                    num_list.insert(low-1, new_elem)
+                    pass
+                if float(num_list[low][index])==data:
+                    num_list.insert(low, new_elem)
+                elif float(num_list[low][index])>data:
+                    num_list.insert(low + 1, new_elem)
+                else:
+                    num_list.insert(low, new_elem)
+                    pass
+                break
+                pass
+            pass
+
+        if high<low:
+            num_list.insert(low, new_elem)
+            break
+            pass
+    return num_list
+
 
 if __name__ == "__main__":
     # excel_read_path = r'C:\Users\duanyaochang\Desktop\temp\2017gj.xls'
     # excel_write_path = r'C:\Users\duanyaochang\Desktop\temp\2017gj222.xls'
     excel_read_path = r'D:\000001.xlsx'
-    excel_write_path = r'D:\000002.xls'
-    # data_list = excel_operation.read_excel(excel_read_path)
-    # print('len(data_list):',len(data_list))
-    # excel_operation.write_excel(excel_write_path,filter_rule2(data_list))
+    excel_write_path2 = r'D:\000002.xls'
+    excel_write_path3 = r'D:\000003.xls'
 
     data_dict = excel_operation.read_excel_all(excel_read_path)
-    # for k, v in data_dict.items():
-    #     print('k:',k)
-    #     print('len v:', len(v))
-    #     pass
-    excel_operation.write_excel_all(excel_write_path, filter_rule4(data_dict))
+    # excel_operation.write_excel_all(excel_write_path2, filter_rule4(data_dict))
+    excel_operation.write_excel_all(excel_write_path3, filter_rule5(data_dict))
+
+
+    # data1 = '12'
+    # data2 = 11
+    # print('type:',type(data1))
+    # print('type2:',type(data2))
+    # print('data1:',isinstance(data1,int))
+    # print('data2:',isinstance(data2,int))
+
     pass
