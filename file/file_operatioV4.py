379c6dcf7992d8e667b07d781d85da77537f2cf4
smileliving <291611696@qq.com>
2018-08-20 10:33:58 +0800

    201808201033

--- file/file_operatioV4.py
+++ file/file_operatioV4.py
@@ -386,37 +386,5 @@ def print_list(list, prefix='', surfix=''):
 
 
 if __name__ == "__main__":
-
-    # dir1_path = 'D:/test_dir/a1'
-    # dir2_path = 'D:/test_dir/b1'
-    # dir1_path = 'H:/file/picture'
-    # dir1_path = 'D:/file-all/file-life'
-    # dir2_path = 'G:/file-life'
-
-    # dir1_path = 'D:/file-all/file-common'
-    # dir2_path = 'G:/file-common'
-
-    dir1_path = 'D:/file-all/file-dev'
-    dir2_path = 'G:/file-dev'
-
-    time1 = datetime.datetime.now()
-    compare_dir(dir1_path, dir2_path)
-    time2 = datetime.datetime.now()
-    print(time1)
-    print(time2)
-
-    # file1 = 'D:/test_dir/1.txt'
-    # with open(file1, 'r', encoding='utf-8') as f:
-    #     for path in f:
-    #         path = path.strip(' ').strip('\n').strip('\ufeff')
-    #         print('path:', path)
-    #         os.remove(path)
-    #         # c = input('y or n')
-    #         # if c == 'y':
-    #         #     os.remove(path)
-    #         #     pass
-    #         pass
-    #     pass
-
     pass
 
--- tools/backup_files.py
+++ tools/backup_files.py
@@ -1,6 +1,43 @@
 # -*- coding: utf-8 -*-
+import datetime
+from file import file_operatioV4
 
 
-def find_diff(dir_path1, dir_path2):
+def find_diff():
+    # dir1_path = 'D:/test_dir/a1'
+    # dir2_path = 'D:/test_dir/b1'
+    # dir1_path = 'H:/file/picture'
+    dir1_path = 'D:/file-all/file-life'
+    # dir2_path = 'G:/file-life'
+    dir2_path = 'F:/all/file-life'
 
+    # dir1_path = 'D:/file-all/file-common'
+    # dir2_path = 'G:/file-common'
+
+    # dir1_path = 'D:/file-all/file-dev'
+    # dir2_path = 'G:/file-dev'
+
+    time1 = datetime.datetime.now()
+    file_operatioV4.compare_dir(dir1_path, dir2_path)
+    time2 = datetime.datetime.now()
+    print(time1)
+    print(time2)
+
+    # file1 = 'D:/test_dir/1.txt'
+    # with open(file1, 'r', encoding='utf-8') as f:
+    #     for path in f:
+    #         path = path.strip(' ').strip('\n').strip('\ufeff')
+    #         print('path:', path)
+    #         os.remove(path)
+    #         # c = input('y or n')
+    #         # if c == 'y':
+    #         #     os.remove(path)
+    #         #     pass
+    #         pass
+    #     pass
+    pass
+
+
+if __name__ == "__main__":
+    find_diff()
     pass
